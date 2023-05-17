142d5ab79654cd20cd2e23ab02cf787be5a2f610
raghulnarayanasami <58605993+raghulnarayanasami@users.noreply.github.com>
2023-05-16 22:12:24 -0400

    updated Accesskey

--- myapp/views.py
+++ myapp/views.py
@@ -68,8 +68,8 @@ def s3bucket(request):
 def boto3connection(bucketname, filename, uploadfile):
 
     host = "https://172.25.200.67:443"
-    access = "08c08f8f09744dd4b9212b6c5d5ce83"
-    secret = "3af80ecdd7774982af7540db1f73fa"
+    access = "08c08f8f09744dd4b9212b6c56d5ce83"
+    secret = "3af80ecdd7774982a26f7540db1f73fa"
     cert_path = "/root/tcs_ecp_cert/tcsecp.pem"
 
     s3 = boto3.resource('s3',endpoint_url=host,verify=False,aws_access_key_id=access,aws_secret_access_key=secret)
@@ -79,8 +79,8 @@ def boto3connection(bucketname, filename, uploadfile):
 
 def bucketlist():
     host = "https://172.25.200.67:443"
-    access = "08c08f8f09744dd4b9212b6c5d5ce83"
-    secret = "3af80ecdd7774982af7540db1f73fa"
+    access = "08c08f8f09744dd4b9212b6c56d5ce83"
+    secret = "3af80ecdd7774982a26f7540db1f73fa"
     cert_path = "/root/tcs_ecp_cert/tcsecp.pem"
 
     s3 = boto3.resource('s3',endpoint_url=host,verify=False,aws_access_key_id=access,aws_secret_access_key=secret)
